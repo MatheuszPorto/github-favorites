d8a1a2c291d08386f4755e252305c4a3c7119800
Matheus Mendonça <109557325+MatheuszPorto@users.noreply.github.com>
2023-03-18 16:43:29 -0300

    feat: starting rows with js (#3)
    * feat: starting rows with js
    
    * feat: rows made with js

--- favorites.js
+++ favorites.js
@@ -1,23 +1,72 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
-    this.update()
+    this.load()
+  }
+
+  load() {
+     this.entries = [
+      {
+      login: 'matheuszporto',
+      name: 'Matheus Mendonça',
+      public_repos: '19',
+      followers: '10',
+      },
+
+      {
+      login: 'maykbrito',
+      name: 'Mayk Brito',
+      public_repos: '76',
+      followers: '120000',
+      }
+    ]
   }
 }
 
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
+    this.tbody = this.root.querySelector("table tbody")
+    this.update()
   }
 
   update() {
     this.removeAllTr()
+
+    this.entries.forEach((user) => {
+      const row = this.createRow()
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+
+      this.tbody.append(row)
+    }) 
   }
   
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td class="user">
+      <img src="https:/github.com/matheuszporto.png" alt="Imagem de Matheus Mendonça">
+      <a href="https://github.com/matheuszporto" target="_blank">
+        <p>Matheus Mendonça</p>
+        <span>matheuszporto</span>
+      </a>
+    </td>
+    <td class="repositories">
+      123
+    </td>
+    <td class="followers">
+      1234
+    </td>
+    <td>
+      <button id="remove-btn">Remove</button>
+    </td>
+    `
+    return tr
+  }
+
   removeAllTr() {
-    const tbody = this.root.querySelector("table tbody")
-  
-    tbody.querySelectorAll('tr').forEach((tr) => {
+    this.tbody.querySelectorAll('tr').forEach((tr) => {
       tr.remove()
     })
   }
